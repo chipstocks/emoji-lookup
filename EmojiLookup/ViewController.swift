c09feacac66dee8029634e25310b5b320b72f4fd
Chip's 2017 Mac <wstocksdale@gmail.com>
2017-05-27 08:40:16 -0400

    Initial Commit

---
+++ EmojiLookup/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EmojiLookup
+//
+//  Created by Chip's 2017 Mac on 5/27/17.
+//  Copyright © 2017 Chip's 2017 Mac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
